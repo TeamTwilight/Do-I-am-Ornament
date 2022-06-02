dcea121e0b3f3296b31542696222e90dbdd6e069
Andromander <androsa@protonmail.com>
2022-06-02 14:02:08 +1000

    Register Slab ornaments

--- src/main/java/com/androsa/doiamornament/ModBlocks.java
+++ src/main/java/com/androsa/doiamornament/ModBlocks.java
@@ -4,6 +4,18 @@ import com.androsa.doiamornament.block.*;
 import com.androsa.ornamental.builder.OrnamentBuilder;
 import com.androsa.ornamental.items.OrnamentBlockItem;
 import com.androsa.ornamental.registry.PropertiesHelper;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class ModBlocks {
 
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, DoIAmOrnamentMod.MODID);
@@ -23,6 +35,20 @@ public class ModBlocks {
 	public static final RegistryObject<OrnamentTFStairs> mining_log_stairs = makeStairs(TFOrnamentBuilders.MINING_LOG);
 	public static final RegistryObject<OrnamentTFStairs> sorting_log_stairs = makeStairs(TFOrnamentBuilders.SORTING_LOG);
 
+	public static final RegistryObject<OrnamentTFSlab> ironwood_slab = makeSlab(TFOrnamentBuilders.IRONWOOD);
+	public static final RegistryObject<OrnamentTFSlab> fiery_slab = makeSlab(TFOrnamentBuilders.FIERY);
+	public static final RegistryObject<OrnamentTFSlab> steeleaf_slab = makeSlab(TFOrnamentBuilders.STEELEAF);
+	public static final RegistryObject<OrnamentTFSlab> arctic_fur_slab = makeSlab(TFOrnamentBuilders.ARCTIC_FUR);
+	public static final RegistryObject<OrnamentTFSlab> carminite_slab = makeSlab(TFOrnamentBuilders.CARMINITE);
+	public static final RegistryObject<OrnamentTFSlab> twilight_oak_log_slab = makeSlab(TFOrnamentBuilders.TWILIGHT_OAK_LOG);
+	public static final RegistryObject<OrnamentTFSlab> canopy_log_slab = makeSlab(TFOrnamentBuilders.CANOPY_LOG);
+	public static final RegistryObject<OrnamentTFSlab> mangrove_log_slab = makeSlab(TFOrnamentBuilders.MANGROVE_LOG);
+	public static final RegistryObject<OrnamentTFSlab> dark_oak_log_slab = makeSlab(TFOrnamentBuilders.DARK_OAK_LOG);
+	public static final RegistryObject<OrnamentTFSlab> time_log_slab = makeSlab(TFOrnamentBuilders.TIME_LOG);
+	public static final RegistryObject<OrnamentTFSlab> transformation_log_slab = makeSlab(TFOrnamentBuilders.TRANSFORMATION_LOG);
+	public static final RegistryObject<OrnamentTFSlab> mining_log_slab = makeSlab(TFOrnamentBuilders.MINING_LOG);
+	public static final RegistryObject<OrnamentTFSlab> sorting_log_slab = makeSlab(TFOrnamentBuilders.SORTING_LOG);
+
 	private static RegistryObject<OrnamentTFStairs> makeStairs(TFOrnamentBuilder builder) {
 		BlockBehaviour.Properties props = PropertiesHelper.createProps(builder.getBuilder());
 		if (!builder.occlusion) props.noOcclusion();
@@ -32,6 +58,14 @@ public class ModBlocks {
 				(item) -> registerBlockItem(item, CreativeModeTab.TAB_BUILDING_BLOCKS, builder, 4));
 	}
 
+	private static RegistryObject<OrnamentTFSlab> makeSlab(TFOrnamentBuilder builder) {
+		BlockBehaviour.Properties props = PropertiesHelper.createProps(builder.getBuilder());
+		if (!builder.occlusion) props.noOcclusion();
+		if (builder.emissive) props.emissiveRendering((state, world, pos) -> true);
+
+		return registerBlock(builder.name + "_slab", () -> new OrnamentTFSlab(props, builder),
+				(item) -> registerBlockItem(item, CreativeModeTab.TAB_BUILDING_BLOCKS, builder, 3));
+	}
 	private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<? extends T> block, Function<RegistryObject<T>, Supplier<? extends Item>> item) {
 		RegistryObject<T> reg = BLOCKS.register(name, block);
 		ITEMS.register(name, item.apply(reg));
