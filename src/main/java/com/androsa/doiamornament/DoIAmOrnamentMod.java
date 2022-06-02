73aee8e57df455dd91f01afdf9b133543a1d3dba
Andromander <androsa@protonmail.com>
2022-06-02 12:09:03 +1000

    Project setup

---
+++ src/main/java/com/androsa/doiamornament/DoIAmOrnamentMod.java
@@ -0,0 +1,13 @@
+package com.androsa.doiamornament;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+@Mod(DoIAmOrnamentMod.MODID)
+public class DoIAmOrnamentMod {
+    public static final String MODID = "doiamornament";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DoIAmOrnamentMod() {
+
+    }
