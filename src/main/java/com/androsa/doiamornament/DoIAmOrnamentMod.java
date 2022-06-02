3f2bbfbdfde59cab3534fbdc9b52d3bff84dde19
Andromander <androsa@protonmail.com>
2022-06-02 12:09:41 +1000

    Spacing

--- src/main/java/com/androsa/doiamornament/DoIAmOrnamentMod.java
+++ src/main/java/com/androsa/doiamornament/DoIAmOrnamentMod.java
@@ -3,6 +3,7 @@ package com.androsa.doiamornament;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 @Mod(DoIAmOrnamentMod.MODID)
 public class DoIAmOrnamentMod {
     public static final String MODID = "doiamornament";
